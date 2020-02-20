b552bf046bf2f9c9621804b6e57b3491737610d7
Sergio <sergiologins92@gmail.com>
2020-02-19 21:54:28 -0500

    segundo cambio

---
+++ 1.py
@@ -0,0 +1,5 @@
+import wx
+
+wx.App().MainLoop()
+wx.Frame(None, title='Avance de graficas').Show()
+
---
+++ nominimize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+# no_minimize.py
+
+import wx
+
+app = wx.App()
+frame = wx.Frame(None, style=wx.CLOSE_BOX)
+frame.Show(True)
+
+app.MainLoop()
